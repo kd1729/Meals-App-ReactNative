001fa29e3949313e09b07e446a22407d9e87926a
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-10-19 17:20:47 +0530

    displaying item in a grid with styles

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,13 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet } from "react-native";
 
 function CategoryGridTile({title, color}) {
   return (
-    <View>
-      <Pressable>
-        <View>
-            <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable android_ripple={{ color: "#ccc" }} style={
+        ({ pressed }) => [styles.button, { opacity: pressed ? 0.5 : 1 }]
+      }>
+        <View style={[styles.innerContainer, {backgroundColor: color}]}>
+            <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +15,27 @@ function CategoryGridTile({title, color}) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 25,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        overflow: "hidden",
+    },
+    button: {
+        flex: 1,
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    }
+});
